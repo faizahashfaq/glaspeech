c94e51742fe553f438764738f20d7b2c11513821
Faiza Ashfaq <30417366+faizahashfaq@users.noreply.github.com>
2022-05-22 16:54:33 +0500

    Update STT.py

--- STT.py
+++ STT.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import json
+from difflib import SequenceMatcher
 
 def STT(filename):
     r = sr.Recognizer()
@@ -9,4 +10,17 @@ def STT(filename):
         audio = r.record(source)
     output = r.recognize_google(audio)
     print(output)
-    return json.dumps(output)
+    files = filename.split(".")
+    target = files[0]
+    target = target.replace("_", " ")
+    target = target.lower()
+    output = output.lower()
+    if output in target:
+        answer = {'Similarity': 100,
+             'Spoken String': output}
+    else:
+        prob = SequenceMatcher(None, output, target).ratio() * 100
+        answer = {'Similarity': prob,
+                  'Spoken String': output}
+
+    return json.dumps(answer)
